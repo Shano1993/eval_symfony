393b17a35dd18568b26d3b8fc4c7f531d5b7ec61
shano1993 <stefan.hanotiau@gmail.com>
2023-03-17 10:05:40 +0100

    disorganized & front

--- src/Controller/DisorganizedController.php
+++ src/Controller/DisorganizedController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +20,21 @@ class DisorganizedController extends AbstractController
 
         return $this->render('disorganized/list.html.twig', [
             'list_disorganized' => $disorganized,
+        ]);
+    }
+
+    #[Route('/disorganized/edit/{id}', name: 'disorganized_edit')]
+    public function edit(Article $article, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('article/add-article.html.twig', [
+            'form_article' => $form->createView(),
         ]);
     }
 }
